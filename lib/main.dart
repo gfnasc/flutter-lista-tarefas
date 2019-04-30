3769a99bdcd7ca13eb6cb649399c2c7b3f0d60f9
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2019-04-30 09:29:36 -0300

    Definindo funções para leitura e escrita dos dados em arquivo JSON a partir de uma List

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'dart:convert';
 
 void main() {
   runApp(
@@ -15,8 +17,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List _todoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try{
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e){
+      return null;
+    }
   }
 }
