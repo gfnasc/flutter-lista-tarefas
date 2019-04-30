309b4f84a8bf5d766109f07efc09455848d44670
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2019-04-30 11:37:57 -0300

    criação da lista ListView CheckboxListTile, salvando item no json, Dismissible e exclusão e SnackBar e undo function

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,135 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  final _todoController = TextEditingController();
+
   List _todoList = [];
+
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
+  @override
+  void initState() {
+    super.initState();
+    _readData().then((data){
+      setState(() {
+        _todoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addTodo() {
+    setState(() {
+      if(_todoController.text == ''){
+
+      } else {
+        Map<String, dynamic> newTodo = Map();
+        newTodo["title"] = _todoController.text;
+        _todoController.text = '';
+        newTodo["ok"] = false;
+        _todoList.add(newTodo);
+        _saveData();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: _todoController,
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(
+                            color: Colors.blueAccent
+                        )
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  color: Colors.blueAccent,
+                  child: Text("ADD"),
+                  textColor: Colors.white,
+                  onPressed: _addTodo,
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _todoList.length,
+                itemBuilder: buildItem
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildItem(context, index){
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_todoList[index]);
+          _lastRemovedPos = index;
+          _todoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text('Tarefa \'${_lastRemoved['title']}\' removida!'),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: (){
+                  setState(() {
+                    _todoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0),
+          child: Icon(Icons.delete, color: Colors.white)
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_todoList[index]["title"]),
+        value: _todoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(
+              _todoList[index]["ok"] ? Icons.check : Icons.error
+          ),
+        ),
+        onChanged: (c){
+          setState(() {
+            _todoList[index]['ok'] = c;
+            _saveData();
+          });
+        },
+      ),
+    );
   }
 
   Future<File> _getFile() async {
